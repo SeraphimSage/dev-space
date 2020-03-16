8e4d200fb3a57630b3c4e60fadc328e229fcdb60
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-03-16 11:42:53 -0600

    Updated dependencies, minor fixes

--- src/react/Profile.js
+++ src/react/Profile.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { Menu, UpdateUserForm } from "./components";
 import { userIsAuthenticated } from "./HOCs";
-import { DeleteUserProfileBtn } from "./components"
+import { DeleteUserProfileBtn } from "./components";
 import "./components/update.css";
 import { UserBadge } from "./components";
 
-
 class Profile extends React.Component {
-  render() {
-    return (
-      <>
-        <Menu isAuthenticated={this.props.isAuthenticated} />
-        <h2 id="profile">Profile</h2>
-        <UserBadge/>
-        <DeleteUserProfileBtn/>
-        <h2 id="update">Update Username or Password</h2>
-        <UpdateUserForm/>
-        </>
-    );
-  }
+	render() {
+		return (
+			<>
+				<Menu isAuthenticated={this.props.isAuthenticated} />
+				<h2 id="profile">Profile</h2>
+				<UserBadge />
+				<DeleteUserProfileBtn />
+				<h2 id="update">Update Username or Password</h2>
+				<UpdateUserForm />
+			</>
+		);
+	}
 }
-
 
 export default userIsAuthenticated(Profile);
