9537a88bafbf7b6878ae07efa3bd1944307622d9
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-03-16 13:17:19 -0600

    Converted profile to Semantic UI Card.

--- src/react/Profile.js
+++ src/react/Profile.js
@@ -6,13 +6,25 @@ import "./components/update.css";
 import { UserBadge } from "./components";
 
 class Profile extends React.Component {
+	state = {
+		username: this.props.match.params.username
+	};
+
+	componentDidUpdate(prevProps) {
+		if (prevProps.location.pathname !== this.props.location.pathname) {
+			this.setState({ username: this.props.match.params.username });
+		}
+	}
 	render() {
 		return (
 			<>
 				<Menu isAuthenticated={this.props.isAuthenticated} />
 				<h2 id="profile">Profile</h2>
-				<UserBadge />
-				<DeleteUserProfileBtn />
+				<UserBadge _username={this.state.username} />
+				<DeleteUserProfileBtn
+					currentUser={JSON.parse(localStorage.login).result.username}
+					_username={this.props.match.params.username}
+				/>
 				<h2 id="update">Update Username or Password</h2>
 				<UpdateUserForm />
 			</>
--- src/react/components/UserBadge.js
+++ src/react/components/UserBadge.js
@@ -1,46 +1,86 @@
-import React from "react"
+import React from "react";
 import { connect } from "react-redux";
 import { getUser } from "../../redux/users";
-import "../components/badge.css";
+import { Card, Icon, Image, Label } from "semantic-ui-react";
+import Spinner from "react-spinkit";
+//import UploadUserPicture from "./UploadUserPicture";
 
 class UserBadge extends React.Component {
-    componentDidMount() {
-        this.props.getUser();
-      }
-      render() {
-        return (
-            <React.Fragment>
-                <div id="badge-div">
-                    <h2>Profile Badge</h2>
-                    <h3>Welcome {this.props.username}</h3>
-                    <h4>Info</h4>
-                    <ul><li>Username: {this.props.result.username}</li> 
-                        <li>Display Name: {this.props.result.displayName}</li>
-                        <li>About: {this.props.result.about}</li>
-                        <li>Created On: {this.props.result.createdAt}</li>                    
-                    </ul>
-                </div>
-                </React.Fragment>
-    )
-    }
-    }
+	componentDidMount() {
+		if (this.props._username) {
+			this.props.getUser(this.props._username);
+		}
+	}
 
-    const mapStateToProps = state => {
-        if (state.users.getUser.result) {
-            return {
-          username: state.users.getUser.result.user.username,
-          result: state.users.getUser.result.user,
-          loading: state.messages.getMessages.loading,
-          error: state.messages.getMessages.error
-        };
-    };
-        return {
-            username: "loading...",
-            result: "loading...",
-            loading: "loading...",
-            error: "loading..."
-        }
+	componentDidUpdate(prevProps) {
+		if (prevProps._username !== this.props._username) {
+			this.props.getUser(this.props._username);
+		}
+	}
+	render() {
+		if (this.props.result === null) {
+			return <Spinner name="cicle" color="orange" />;
+		}
+		const user = this.props.result.user;
+		return (
+			<React.Fragment>
+				<Card>
+					<Image
+						src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+						wrapped
+						ui={false}
+						maxwidth="20em"
+					/>
+					<Card.Content>
+						<Card.Header>{this.props.username} </Card.Header>
+						<Card.Meta>
+							<span>
+								<Label>
+									<Icon name="at" />
+									{this.props.username}
+								</Label>
+							</span>
+						</Card.Meta>
+						<Card.Meta>
+							<span className="date">
+								Created: {new Date(this.props.result.createdAt).toDateString()}
+							</span>
+						</Card.Meta>
+						<Card.Description>
+							{this.props.about
+								? this.props.about
+								: "Stay tuned for the about details"}{" "}
+						</Card.Description>
+					</Card.Content>
+					<Card.Content extra></Card.Content>
+				</Card>
+			</React.Fragment>
+		);
+	}
 }
-      const mapDispatchToProps = { getUser };
-      export default connect(mapStateToProps, mapDispatchToProps)(UserBadge);
-      
+
+const mapStateToProps = state => {
+	if (state.users.getUser.result) {
+		return {
+			loggedIn: state.auth.login.result.username,
+			username: state.users.getUser.result.user.username,
+			pictureLocation: state.users.getUser.result.user.pictureLocation,
+			displayName: state.users.getUser.result.user.displayName,
+			about: state.users.getUser.result.user.about,
+			googleId: state.users.getUser.result.user.googleId,
+			createdAt: state.users.getUser.result.user.createdAt,
+			updatedAt: state.users.getUser.result.user.updatedAt,
+			result: state.users.getUser.result.user,
+			loading: state.messages.getMessages.loading,
+			error: state.messages.getMessages.error
+		};
+	}
+	return {
+		username: "loading...",
+		result: "loading...",
+		loading: "loading...",
+		error: "loading..."
+	};
+};
+const mapDispatchToProps = { getUser };
+export default connect(mapStateToProps, mapDispatchToProps)(UserBadge);
