7f489bf21da04d0f4bb9cf28e2ade48df68c0ac0
Paul Racisz <paulieracisz@gmail.com>
2020-03-23 10:46:18 -0400

    added click handler

--- src/react/components/AddLike.js
+++ src/react/components/AddLike.js
@@ -2,26 +2,38 @@ import React from 'react';
 // import { userIsAuthenticated } from '../HOCs';
 import { Button, Icon, Label } from 'semantic-ui-react';
 import { connect } from 'react-redux';
-import { addLike } from '../../redux'
+import { addLike, getMessages } from '../../redux'
 
 class AddLike extends React.Component {
+
+    handleAddLike = (e, messageId) => {
+        console.log("treytrey")
+        e.preventDefault();
+        this.props.addLike(messageId);
+    }
+
 	render() {
 		return (
-			<Button as='div' labelPosition='right'>
-                <Button icon>
+			<Button key={message.id} as='div' labelPosition='right'>
+                <Button icon onClick={this.handleAddLike}>
                     <Icon name='heart' />
                         Like
                 </Button>
                 <Label as='a' basic pointing='left'>
-                    2,048
+                    {}
                 </Label>
             </Button>
         );
 	}
 }
 
-export default connect(
-    null,
-    { addLike }
-  )(AddLike);
+const mapStateToProps = state => {
+	return {
+		result: state.likes.addLike.result,
+		loading: state.likes.addLike.loading,
+		error: state.likes.addLike.error
+	};
+};
+const mapDispatchToProps = { addLike, getMessages };
 
+export default connect(mapStateToProps, mapDispatchToProps)(AddLike);
