bd2507ca12a49fec6b69354870eb4a4c2b351804
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-03-24 12:25:13 -0600

    update code sytling.

--- src/react/Profile.js
+++ src/react/Profile.js
@@ -5,27 +5,25 @@ import "./components/update.css";
 import { UserBadge } from "./components";
 
 class Profile extends React.Component {
-
-	render() {
-		return (
-			<>
-				<Menu isAuthenticated={this.props.isAuthenticated} />
-				<div id="row">
-					<div className="column" id="placeHolder"></div>
-					<div className="column" id="mainBody">
-						<h2 id="profile">Profile</h2>
-						<UserBadge />
-						<ConditionalUpdate />
-						
-					</div>
-					<div className="column" id="getUserList">
-						<h3>Check out our newest users!</h3>
-						<GetUserList />
-					</div>
-				</div>
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        <Menu isAuthenticated={this.props.isAuthenticated} />
+        <div id="row">
+          <div className="column" id="placeHolder"></div>
+          <div className="column" id="mainBody">
+            <h2 id="profile">Profile</h2>
+            <UserBadge />
+            <ConditionalUpdate />
+          </div>
+          <div className="column" id="getUserList">
+            <h3>Check out our newest users!</h3>
+            <GetUserList />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default userIsAuthenticated(Profile);
